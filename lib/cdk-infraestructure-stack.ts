9a1b78dcc18785a547289ca41572ec95ec31c002
William Avila <willocoav@example.com>
2023-03-06 19:26:01 -0500

    first commit descrition and env variables

--- lib/cdk-infraestructure-stack.ts
+++ lib/cdk-infraestructure-stack.ts
@@ -1,16 +1,41 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+
+
+const ENVIRONMENT_VARIABLES = {
+  NEXT_PUBLIC_SQL_USER : 'admin',
+  NEXT_PUBLIC_SQL_PASSWORD:'12345678',
+  NEXT_PUBLIC_SQL_SERVER: 'litethinking.c3xymb4w4f8t.us-east-1.rds.amazonaws.com',
+  NEXT_PUBLIC_SQL_DATABASE: 'LITE_THINKING'
+}
 
 export class CdkInfraestructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "myVPC", {
+      availabilityZones:['us-east-1a','us-east-1b'],
+      vpcName: 'LiteVPC'
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkInfraestructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc,
+      clusterName:'liteThinkVPC'
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService2", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 2, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("public.ecr.aws/p1g5b2e2/think-lite"), environment:ENVIRONMENT_VARIABLES },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is true
+      
+    });
   }
 }
